b14bf27cc8e3cc2f21d34fe92ffd25f754d16dee
Usman <musman@gomarkho.com>
2023-06-12 12:36:48 +0500

    Initial setup - Generate app using ts template - Add product component

---
+++ components/Product.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { FunctionComponent } from "react";
+
+interface ProductProps {
+
+}
+
+const Product: FunctionComponent<ProductProps> = () => {
+    return (
+        <>
+            <h1>Procutct Dashboard</h1>
+        </>
+    );
+}
+
+export default Product;
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Product from "./../components/Product"
+
+function App() {
+  return (
+    <div className="App">
+      <Product/>
+    </div>
+  );
+}
+
+export default App;
